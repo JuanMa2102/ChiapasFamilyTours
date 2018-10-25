31e3dd2eb2f045e16b7b0fdd1ab86ca2075d74fa
JuanraCamacho <juanch00@live.com.mx>
2018-10-24 21:20:41 -0500

    JuanRa
    Traslados No terminado xd

--- app/Http/Controllers/AdminTrasladosController.php
+++ app/Http/Controllers/AdminTrasladosController.php
@@ -8,8 +8,15 @@ use DB;
 class AdminTrasladosController extends Controller
 {
     function index(){
-        
-        return view('adminViews.Traslados.create');
+        $traslados = DB::table('tbl_traslados')
+        ->where('activo','=',1)
+        ->get();
+        $trasladosdetalle = DB::table('tbl_trasladosdetalle')
+        ->where('activo','=',1)
+        ->get();
+        return view('adminViews.Traslados.index',["traslados"=>$traslados,
+                                                    "trasladosdetalle"=>$trasladosdetalle,                                            
+                                                    "id"=>1]);
     }
 
     public function store(Request $request)
